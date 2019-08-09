bcf9b88f633de2b77d14ed355dc0adfcf2001719
Christophe MASSOLIN <christophe.massolin@makemepulse.com>
2019-08-09 15:35:26 +0200

    Implement RAF

---
+++ js/dom/raf.js
@@ -0,0 +1,117 @@
+"use strict";
+/**
+ * raf.js
+ *
+ * Global RequestAnimationFrame
+ *
+ * ----------------------------
+ *
+ * use example
+ *
+ * var RAF = require('./libs/raf)'
+ *
+ * RAF.subscribe( 'mySubscriberId', mySubscriberFn )
+ * RAF.unsubscribe( 'mySubscriberId' )
+ * RAF.start()
+ * RAF.stop()
+ */
+var __read = (this && this.__read) || function (o, n) {
+    var m = typeof Symbol === "function" && o[Symbol.iterator];
+    if (!m) return o;
+    var i = m.call(o), r, ar = [], e;
+    try {
+        while ((n === void 0 || n-- > 0) && !(r = i.next()).done) ar.push(r.value);
+    }
+    catch (error) { e = { error: error }; }
+    finally {
+        try {
+            if (r && !r.done && (m = i["return"])) m.call(i);
+        }
+        finally { if (e) throw e.error; }
+    }
+    return ar;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var requestAnimFrame = (function () {
+    return window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        // @ts-ignore
+        window.mozRequestAnimationFrame ||
+        function (callback) {
+            window.setTimeout(callback, 1000 / 60);
+        };
+})();
+var cancelRequestAnimFrame = (function () {
+    return window.cancelAnimationFrame ||
+        // @ts-ignore
+        window.webkitCancelRequestAnimationFrame ||
+        // @ts-ignore
+        window.mozCancelRequestAnimationFrame ||
+        // @ts-ignore
+        window.oCancelRequestAnimationFrame ||
+        // @ts-ignore
+        window.msCancelRequestAnimationFrame;
+})();
+var RAF = /** @class */ (function () {
+    function RAF() {
+    }
+    /**
+     * Run all subscribers
+     */
+    RAF._update = function () {
+        RAF._now = Date.now();
+        RAF.dt = RAF._now - RAF._lt;
+        RAF._elapsedInterval += RAF.dt;
+        if (RAF._elapsedInterval >= RAF.framerate) {
+            RAF._elapsedInterval = 0;
+            RAF._processUpdate();
+        }
+        RAF._lt = RAF._now;
+        RAF._raf = requestAnimFrame(RAF._update);
+    };
+    RAF._processUpdate = function () {
+        for (var i = 0; i < RAF.subscribers.length; i++) {
+            var _a = __read(RAF.subscribers[i], 2), _ = _a[0], subscriber = _a[1];
+            // execute handler
+            subscriber();
+        }
+    };
+    /**
+     * Register a new subscriber
+     */
+    RAF.subscribe = function (id, fn) {
+        RAF.subscribers.push([id, fn]);
+    };
+    /**
+    * Unregister a subscriber
+    */
+    RAF.unsubscribe = function (id) {
+        for (var i = 0; i < RAF.subscribers.length; i++) {
+            // if id matches, removes
+            if (RAF.subscribers[i][0] === id) {
+                RAF.subscribers.splice(i, 1);
+            }
+        }
+    };
+    /**
+     * Start globally the RAF
+     */
+    RAF.start = function () {
+        RAF._raf = requestAnimFrame(RAF._update);
+    };
+    /**
+     * Stop globally the RAF
+     */
+    RAF.stop = function () {
+        cancelRequestAnimFrame(RAF._raf);
+    };
+    RAF.subscribers = [];
+    RAF.dt = 0;
+    RAF.framerate = 16;
+    RAF._now = Date.now();
+    RAF._lt = RAF._now;
+    RAF._elapsedInterval = 0;
+    RAF._raf = requestAnimFrame(RAF._update);
+    return RAF;
+}());
+exports.RAF = RAF;
---
+++ lib/dom/raf.ts
@@ -0,0 +1,119 @@
+/**
+ * raf.js
+ *
+ * Global RequestAnimationFrame
+ *
+ * ----------------------------
+ *
+ * use example
+ *
+ * var RAF = require('./libs/raf)'
+ *
+ * RAF.subscribe( 'mySubscriberId', mySubscriberFn )
+ * RAF.unsubscribe( 'mySubscriberId' )
+ * RAF.start()
+ * RAF.stop()
+ */
+
+const requestAnimFrame = (function () {
+  return window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    // @ts-ignore
+    window.mozRequestAnimationFrame ||
+    function (callback) {
+      window.setTimeout(callback, 1000 / 60);
+    };
+  })();
+
+
+  const cancelRequestAnimFrame = (function () {
+    return window.cancelAnimationFrame ||
+    // @ts-ignore
+    window.webkitCancelRequestAnimationFrame ||
+    // @ts-ignore
+    window.mozCancelRequestAnimationFrame ||
+    // @ts-ignore
+    window.oCancelRequestAnimationFrame ||
+    // @ts-ignore
+    window.msCancelRequestAnimationFrame
+})();
+
+export type Subscriber = [ string, () => void ]
+
+export class RAF {
+
+  static subscribers: Subscriber[] = []
+  static dt = 0
+  static framerate = 16
+
+  private static _now = Date.now();
+  private static _lt = RAF._now;
+  private static _elapsedInterval = 0;
+  private static _raf = requestAnimFrame(RAF._update)
+
+  /**
+   * Run all subscribers
+   */
+  private static _update() {
+
+    RAF._now = Date.now()
+
+    RAF.dt = RAF._now - RAF._lt
+
+    RAF._elapsedInterval += RAF.dt
+
+    if (RAF._elapsedInterval >= RAF.framerate) {
+      RAF._elapsedInterval = 0
+      RAF._processUpdate()
+    }
+
+    RAF._lt = RAF._now
+    RAF._raf = requestAnimFrame(RAF._update)
+
+  }
+
+  private static _processUpdate() {
+
+    for (var i = 0; i < RAF.subscribers.length; i++) {
+      var [ _, subscriber ] = RAF.subscribers[i]
+      // execute handler
+      subscriber()
+    }
+
+  }
+
+  /**
+   * Register a new subscriber
+   */
+  static subscribe(id: string, fn: () => void) {
+    RAF.subscribers.push([id, fn])
+  }
+
+  /**
+  * Unregister a subscriber
+  */
+  static unsubscribe(id: string) {
+
+    for (var i = 0; i < RAF.subscribers.length; i++) {
+      // if id matches, removes
+      if (RAF.subscribers[i][0] === id) {
+        RAF.subscribers.splice(i, 1)
+      }
+    }
+
+  }
+
+  /**
+   * Start globally the RAF
+   */
+  static start() {
+    RAF._raf = requestAnimFrame(RAF._update)
+  }
+
+  /**
+   * Stop globally the RAF
+   */
+  static stop() {
+    cancelRequestAnimFrame(RAF._raf)
+  }
+}
