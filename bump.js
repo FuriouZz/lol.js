b7be19fdc4ca79c8346e093d9f0ac7f34a150111
Christophe Massolin <christophe.massolin@gmail.com>
2022-04-25 22:51:08 +0200

    update bump

--- bump.js
+++ bump.js
@@ -1,46 +1,69 @@
-const { editFile, readFile, writeFile } = require("./dist/node/fs");
-const inc = require("semver/functions/inc")
-const { spawnSync } = require("child_process")
+const { readFileSync, writeFileSync } = require("fs");
+const inc = require("semver/functions/inc");
+const { spawnSync } = require("child_process");
 
-const Options = { shell: true, stdio: 'inherit' }
+const Options = { shell: true, stdio: "inherit" };
 
-async function _bump(release = "patch") {
-  let version = null
-
-  await editFile("package.json", (buf) => {
-    const pkg = JSON.parse(buf.toString("utf-8"))
-    version = inc(pkg.version, release)
-
-    if (version === null) {
-      throw new Error(`Bump failed`)
-    }
-
-    pkg.version = version
-    return JSON.stringify(pkg, null, 2)
-  })
-
-  spawnSync("npm install", Options)
-  spawnSync(`git commit -am "Bump ${version}" && git tag ${version} && git push --tags && git push`, Options)
+/**
+ *
+ * @param {string} version
+ */
+function bump(version) {
+  spawnSync("npm install", Options);
+  spawnSync(
+    `git commit -am "Bump ${version}" && git tag ${version} && git push --tags && git push`,
+    Options
+  );
 }
 
-async function main() {
-  const release = process.argv[2] || "patch"
+function getPackage(release = "patch", identifier = undefined) {
+  const pkg = JSON.parse(readFileSync("package.json", { encoding: "utf-8" }));
+  const nextVersion = inc(pkg.version, release, undefined, identifier);
+  return {
+    pkg,
+    currentVersion: pkg.version,
+    nextVersion,
+    save() {
+      pkg.version = nextVersion;
+      writeFileSync("package.json", JSON.stringify(pkg, null, 2));
+    },
+  };
+}
 
-  const buf0 = await readFile("package.json")
-  const pkg0 = JSON.parse(buf0.toString("utf-8"))
-  const buf1 = await readFile("package-lock.json")
-  const pkg1 = JSON.parse(buf1.toString("utf-8"))
+const getRelease = () => {
+  const index = process.argv.findIndex((a) =>
+    /^(pre)?(patch|minor|major|release)/.test(a)
+  );
+  if (index === -1) return { release: "patch", identifier: undefined };
+  const [release, identifier] = process.argv[index].split("-");
+  return { release: identifier ? `pre${release}` : release, identifier };
+};
+
+async function main() {
+  const dryRun = process.argv.includes("--dry-run");
+  const { release, identifier } = getRelease();
+
+  const buf0 = readFileSync("package.json");
+  const pkg0 = JSON.parse(buf0.toString("utf-8"));
+  const buf1 = readFileSync("package-lock.json");
+  const pkg1 = JSON.parse(buf1.toString("utf-8"));
 
   try {
-    await _bump(release)
+    const pkg = getPackage(release, identifier);
+    console.log(`Current version: ${pkg.currentVersion}`);
+    console.log(`Next version: ${pkg.nextVersion}`);
+    if (!dryRun) {
+      pkg.save();
+      bump(pkg.nextVersion);
+    }
   } catch (e) {
     // Restore version
-    await writeFile(JSON.stringify(pkg0, null, 2), "package.json")
-    await writeFile(JSON.stringify(pkg1, null, 2), "package-lock.json")
-    console.log(e)
-    process.exit(1)
+    writeFileSync(JSON.stringify(pkg0, null, 2), "package.json");
+    writeFileSync(JSON.stringify(pkg1, null, 2), "package-lock.json");
+    spawnSync("npm install", Options);
+    console.log(e);
+    process.exit(1);
   }
 }
 
-main()
-  .then(null, console.log)
+main().then(null, console.log);
