112664e1b36dec4931eec04c2bf7e37fdeb08831
Christophe Massolin <christophe.massolin@gmail.com>
2022-05-18 22:11:19 +0200

    string: add toPascalCase

--- lib/string/string.ts
+++ lib/string/string.ts
@@ -34,6 +34,10 @@ export function toCamelCase(str: string) {
     .join("");
 }
 
+export function toPascalCase(str: string) {
+  return toUCFirst(toCamelCase(str));
+}
+
 /**
  * Slugify a string and replace tiret to underscore
  */
