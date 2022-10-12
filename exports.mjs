b45d0efd3b63db7ad93d603e9367a4c4ac139b7a
Christophe Massolin <christophe.massolin@gmail.com>
2022-10-12 23:08:56 +0200

    rewrite exports

--- exports.mjs
+++ exports.mjs
@@ -1,4 +1,4 @@
-import { basename, extname, join } from "path";
+import { join } from "path";
 import { readdirSync, readFileSync, statSync, writeFileSync } from "fs";
 
 const isFile = (path) => {
@@ -13,44 +13,50 @@ const dirs = readdirSync("./lib", { withFileTypes: false });
 
 const exports = {
   ".": {
-    source: `./lib/index.ts`,
-    import: `./dist/esm/index.js`,
-    require: `./dist/cjs/index.js`,
-    types: `./dist/types/index.d.ts`,
+    "import": {
+      types: `./dist/esm/index.d.ts`,
+      default: `./dist/esm/index.js`
+    },
+    "require": {
+      types: `./dist/cjs/index.d.ts`,
+      default: `./dist/cjs/index.js`
+    }
   },
   "./*": {
-    source: `./lib/*`,
-    import: `./dist/esm/*`,
-    require: `./dist/cjs/*`,
-    types: `./dist/types/*`
+    "import": {
+      types: `./dist/esm/*.d.ts`,
+      default: `./dist/esm/*.js`
+    },
+    "require": {
+      types: `./dist/cjs/*.d.ts`,
+      default: `./dist/cjs/*.js`
+    }
   },
 };
 
 for (let dir of dirs) {
   if (!isFile(join("./lib", dir, "index.ts"))) continue;
   exports[`./${dir}`] = {
-    source: `./lib/${dir}/index.ts`,
-    import: `./dist/esm/${dir}/index.js`,
-    require: `./dist/cjs/${dir}/index.js`,
-    types: `./dist/types/${dir}/index.d.ts`,
+    "import": {
+      types: `./dist/esm/${dir}/index.d.ts`,
+      default: `./dist/esm/${dir}/index.js`,
+    },
+    "require": {
+      types: `./dist/cjs/${dir}/index.d.ts`,
+      default: `./dist/cjs/${dir}/index.js`,
+    }
   };
-  const files = readdirSync(join("./lib", dir));
-  for (const file of files) {
-    if (!isFile(join("./lib", dir, file))) continue;
-    const name = basename(file, extname(file));
-    exports[`./${dir}/${name}`] = {
-      source: `./lib/${dir}/${name}.ts`,
-      import: `./dist/esm/${dir}/${name}.js`,
-      require: `./dist/cjs/${dir}/${name}.js`,
-      types: `./dist/types/${dir}/${name}.d.ts`,
-    };
-    exports[`./${dir}/${name}.js`] = {
-      source: `./lib/${dir}/${name}.ts`,
-      import: `./dist/esm/${dir}/${name}.js`,
-      require: `./dist/cjs/${dir}/${name}.js`,
-      types: `./dist/types/${dir}/${name}.d.ts`,
-    };
-  }
+
+  exports[`./${dir}/*`] = {
+    "import": {
+      types: `./dist/esm/${dir}/*.d.ts`,
+      default: `./dist/esm/${dir}/*.js`,
+    },
+    "require": {
+      types: `./dist/cjs/${dir}/*.d.ts`,
+      default: `./dist/cjs/${dir}/*.js`,
+    }
+  };
 }
 
 const pkg = JSON.parse(readFileSync("package.json", "utf-8"));
